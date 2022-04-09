beba18ac52e3c0f0acf38c83952a4e100da999f3
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-04-09 21:47:21 +0300

    Create send_image.py

---
+++ telegram/send_image.py
@@ -0,0 +1,17 @@
+# Send an image to [chat_id] using [bot_id]
+# Послать картинку в Телеграм чат [chat_id]
+# используя бот [bot_id]
+
+import requests
+import time
+
+base_url = "https://api.telegram.org/bot[bot_id]/sendPhoto"
+
+parameters = {
+    "chat_id" : "-[chat_id]",
+    "photo" : "[image https]",
+    "caption" : "description"
+}
+
+resp = requests.get(base_url, data = parameters)
+print(resp.text)
